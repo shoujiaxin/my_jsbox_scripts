b72c8a5e706c3eb9b61b4d77174f40e2ba4948d8
Jiaxin Shou <shoujiaxin@gmail.com>
2018-12-21 20:42:18 +0800

    Upgrade Punch In to 0.4.0

--- punch_in/punch_in.js
+++ punch_in/punch_in.js
@@ -1,10 +1,12 @@
 // $app.validEnv = $env.app
-const currVersion = "0.3.3"  // 版本号
+const currVersion = "0.4.0"  // 版本号
 checkUpdate()
-var dayCnt = 0         // 总打卡天数
-var historyCnt = 0     // 总打卡记录数
-var totalDuration = 0  // 总打卡时长
-var daysPerPage = 10   // 每页加载 10 天的记录
+var totalDate = 0
+var totalDuration = 0
+var datePerPage = 10
+var dateCnt = datePerPage
+var historyCnt = 0
+getTotalDuration()
 
 $ui.render({
     props: {
@@ -30,21 +32,13 @@ $ui.render({
                 let year = date.getFullYear()
                 let month = formatDateAndTime(date.getMonth() + 1)
                 let day = formatDateAndTime(date.getDate())
-                let dateStr = `${year}-${month}-${day}`
                 let hour = formatDateAndTime(date.getHours())
                 let min = formatDateAndTime(date.getMinutes())
+                let dateStr = `${year}-${month}-${day}`
                 let timeStr = `${hour}:${min}`
 
-                let db = $sqlite.open("punch_history.db")
-                db.update("CREATE TABLE History(date text, time text)")
-                db.update({
-                    sql: "INSERT INTO History values(?, ?)",
-                    args: [dateStr, timeStr]
-                })
-                db.close()
-                $ui.toast(`${dateStr} ${timeStr} 已打卡`)
-
-                updateHistory()
+                addHistoryData(dateStr, timeStr)
+                $ui.toast(`已打卡`)
             }
         }
     }, {
@@ -64,7 +58,7 @@ $ui.render({
         props: {
             id: "historyList",
             bgcolor: $color("clear"),
-            data: getHistory(dayCnt, daysPerPage),
+            data: getHistoryData(0, datePerPage),
             showsVerticalIndicator: false,
             stickyHeader: true,
             footer: {
@@ -84,7 +78,8 @@ $ui.render({
                     handler: function (sender, indexPath) {
                         let date = sender.data[indexPath.section].title.split("（")[0]
                         let time = sender.data[indexPath.section].rows[indexPath.row]
-                        deleteHistory(date, time)
+
+                        deleteHistoryData(date, time)
                     }
                 },
                 {
@@ -92,7 +87,8 @@ $ui.render({
                     handler: function (sender, indexPath) {
                         let date = sender.data[indexPath.section].title.split("（")[0]
                         let time = sender.data[indexPath.section].rows[indexPath.row]
-                        editHistory(date, time)
+
+                        editHistoryData(date, time)
                     }
                 }
             ]
@@ -105,16 +101,20 @@ $ui.render({
         events: {
             pulled: function (sender) {
                 sender.beginRefreshing()
-                updateHistory()
+                updateHistoryData()
                 $delay(0.5, function () {
                     sender.endRefreshing()
                 })
             },
             didReachBottom: function (sender) {
                 $ui.loading(true)
-                let newData = getHistory(dayCnt, daysPerPage)
+
+                let newHistoryData = getHistoryData(dateCnt, datePerPage)
+                dateCnt += datePerPage
                 sender.endFetchingMore()
-                sender.data = sender.data.concat(newData)
+                sender.data = sender.data.concat(newHistoryData)
+
+                sender.footer.text = `最近 ${historyCnt} 条记录`
                 $ui.loading(false)
             }
         }
@@ -122,7 +122,7 @@ $ui.render({
         type: "label",
         props: {
             id: "totalDurationLabel",
-            text: `共 ${dayCnt} 天，${totalDuration.toFixed(2)} 小时`,
+            text: `共 ${totalDate} 天，${totalDuration.toFixed(2)} 小时`,
             font: $font(16),
             align: $align.center,
             bgcolor: $color("tint")
@@ -141,31 +141,49 @@ function formatDateAndTime(value) {
     return value
 }
 
-function getHistory(dayBegin, dayNum) {
+function getTotalDuration() {
+    totalDate = 0
+    totalDuration = 0
+
+    let db = $sqlite.open("punch_in.db")
+    let object = db.query("SELECT duration FROM Duration")
+    if (object.error) {
+        db.update("CREATE TABLE Duration(date text, duration float)")
+        db.update("CREATE TABLE History(date text, time text)")
+    } else {
+        let result = object.result
+        while (result.next()) {
+            totalDate++
+            totalDuration += result.get("duration")
+        }
+        result.close()
+    }
+    db.close()
+}
+
+function getHistoryData(dayBegin, dayNum) {
     let historyData = []
     let dateList = []
+    let durationList = []
 
-    let db = $sqlite.open("punch_history.db")
+    let db = $sqlite.open("punch_in.db")
     let object = db.query({
-        sql: "SELECT DISTINCT date FROM History ORDER BY date DESC LIMIT ?,?",
+        sql: "SELECT * FROM Duration ORDER BY date DESC LIMIT ?,?",
         args: [dayBegin, dayNum]  // 前者为偏移量；后者为限制值，负数表示无限制
     })
-    if (object.error) {
-        db.close()
-        return
-    }
     let result = object.result
     while (result.next()) {
         dateList.push(result.get("date"))
-        dayCnt++
+        durationList.push(result.get("duration"))
     }
     result.close()
 
     for (let i = 0; i < dateList.length; i++) {
         let item = {
-            title: dateList[i],
+            title: `${dateList[i]}（共 ${durationList[i]} 小时）`,
             rows: []
         }
+
         object = db.query({
             sql: "SELECT time FROM History WHERE date = ? ORDER BY time ASC",
             args: [dateList[i]]
@@ -176,27 +194,29 @@ function getHistory(dayBegin, dayNum) {
             historyCnt++
         }
         result.close()
-        let duration = getDuration(dateList[i], item.rows)
-        totalDuration += parseFloat(duration)
-        item.title += `（共 ${duration} 小时）`
-        historyData.push(item)
+
+        if (item.rows.length != 0) {
+            historyData.push(item)
+        }
     }
     db.close()
+
     return historyData
 }
 
-function updateHistory() {
-    $ui.loading(true)
-    dayCnt = 0
-    historyCnt = 0
-    totalDuration = 0
-    $("historyList").data = getHistory(dayCnt, daysPerPage)
-    $("historyList").footer.text = `最近 ${historyCnt} 条记录`
-    $("totalDurationLabel").text = `共 ${dayCnt} 天，${totalDuration.toFixed(2)} 小时`
-    $ui.loading(false)
+function addHistoryData(date, time) {
+    let db = $sqlite.open("punch_in.db")
+    db.update({
+        sql: "INSERT INTO History values(?, ?)",
+        args: [date, time]
+    })
+    db.close()
+
+    updateDuration(date)
+    updateHistoryData()
 }
 
-function editHistory(date, time) {
+function editHistoryData(date, time) {
     $ui.push({
         props: {
             title: "修改",
@@ -284,15 +304,18 @@ function editHistory(date, time) {
                             title: "确定",
                             disabled: false,
                             handler: function () {
-                                let db = $sqlite.open("punch_history.db")
+                                let db = $sqlite.open("punch_in.db")
                                 db.update({
                                     sql: "UPDATE History SET date = ?, time = ? WHERE date = ? AND time = ?",
                                     args: [$("dateInput").text, $("timeInput").text, date, time]
                                 })
                                 db.close()
 
+                                updateDuration(date)
+                                updateDuration($("dateInput").text)
+                                updateHistoryData()
+
                                 $ui.pop()
-                                updateHistory()
                                 $ui.toast("已修改")
                             }
                         }]
@@ -309,7 +332,7 @@ function editHistory(date, time) {
     })
 }
 
-function deleteHistory(date, time) {
+function deleteHistoryData(date, time) {
     $ui.alert({
         title: "确定要删除吗？",
         actions: [{
@@ -321,54 +344,106 @@ function deleteHistory(date, time) {
             title: "确定",
             disabled: false,
             handler: function () {
-                let db = $sqlite.open("punch_history.db")
+                let db = $sqlite.open("punch_in.db")
                 db.update({
                     sql: "DELETE FROM History WHERE date = ? AND time = ?",
                     args: [date, time]
                 })
                 db.close()
 
-                updateHistory()
+                updateDuration(date)
+                updateHistoryData()
+
                 $ui.toast("已删除")
             }
         }]
     })
 }
 
-function getDuration(date, timeList) {
-    let result = 0
-    let currIndex = 0
-    let startTime = timeList[currIndex]
+function updateHistoryData() {
+    $ui.loading(true)
 
-    // 第一段：06:00~12:00
-    if ("06:00" <= startTime && startTime < "12:00") {
-        while (currIndex < timeList.length && timeList[currIndex] < "12:00") {
-            currIndex++
-        }
-        result += calculateMinutes(date, startTime, timeList[currIndex - 1])
-        if (currIndex < timeList.length) {
-            startTime = timeList[currIndex]
-        }
+    dateCnt = datePerPage
+    historyCnt = 0
+    $("historyList").data = getHistoryData(0, datePerPage)
+    $("historyList").footer.text = `最近 ${historyCnt} 条记录`
+    getTotalDuration()
+    $("totalDurationLabel").text = `共 ${totalDate} 天，${totalDuration.toFixed(2)} 小时`
+
+    $ui.loading(false)
+}
+
+function updateDuration(date) {
+    let timeList = []
+
+    let db = $sqlite.open("punch_in.db")
+    let object = db.query({
+        sql: "SELECT time FROM History WHERE date = ? ORDER BY time ASC",
+        args: [date]
+    })
+    let result = object.result
+    while (result.next()) {
+        timeList.push(result.get("time"))
     }
-    // 第二段：12:00~17:00
-    if ("12:00" <= startTime && startTime < "17:00") {
-        while (currIndex < timeList.length && timeList[currIndex] < "17:00") {
-            currIndex++
+    result.close()
+
+    if (timeList.length == 0) {
+        db.update({
+            sql: "DELETE FROM Duration WHERE date = ?",
+            args: [date]
+        })
+    } else {
+        let currIndex = 0
+        let startTime = timeList[currIndex]
+        let duration = 0
+
+        // 第一段：06:00~12:00
+        if ("06:00" <= startTime && startTime < "12:00") {
+            while (currIndex < timeList.length && timeList[currIndex] < "12:00") {
+                currIndex++
+            }
+            duration += calculateMinutes(date, startTime, timeList[currIndex - 1])
+            if (currIndex < timeList.length) {
+                startTime = timeList[currIndex]
+            }
         }
-        result += calculateMinutes(date, startTime, timeList[currIndex - 1])
-        if (currIndex < timeList.length) {
-            startTime = timeList[currIndex]
+        // 第二段：12:00~17:00
+        if ("12:00" <= startTime && startTime < "17:00") {
+            while (currIndex < timeList.length && timeList[currIndex] < "17:00") {
+                currIndex++
+            }
+            duration += calculateMinutes(date, startTime, timeList[currIndex - 1])
+            if (currIndex < timeList.length) {
+                startTime = timeList[currIndex]
+            }
         }
+        // 第三段：17:00~23:00
+        if ("17:00" <= startTime && startTime <= "23:00") {
+            while (currIndex < timeList.length && timeList[currIndex] < "23:00") {
+                currIndex++
+            }
+            duration += calculateMinutes(date, startTime, timeList[currIndex - 1])
+        }
+        duration = (duration / 60).toFixed(2)  // 单位为小时
+
+        object = db.query({
+            sql: "SELECT * FROM Duration WHERE date = ?",
+            args: [date]
+        })
+        if (object.result.next()) {
+            db.update({
+                sql: "UPDATE Duration SET duration = ? WHERE date = ?",
+                args: [duration, date]
+            })
+        } else {
+            db.update({
+                sql: "INSERT INTO Duration values(?, ?)",
+                args: [date, duration]
+            })
+        }
+        object.result.close()
     }
-    // 第三段：17:00~23:00
-    if ("17:00" <= startTime && startTime <= "23:00") {
-        while (currIndex < timeList.length && timeList[currIndex] < "23:00") {
-            currIndex++
-        }
-        result += calculateMinutes(date, startTime, timeList[currIndex - 1])
-    }
-    result = (result / 60).toFixed(2)  // 单位为小时
-    return result
+    db.close()
 }
 
 function calculateMinutes(dateStr, timeBegin, timeEnd) {
